7c187bd3fedff3f4dec9cbdff1889f92974d2d8a
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-25 13:48:59 +0530

    [SaiVijayaKumar] Instial Commit

---
+++ RegexUsingLamda/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexUsingLamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome to Regex Problems");
+
+            Console.Write("\n Enter First Name : ");
+            string firstName = Convert.ToString(Console.ReadLine());
+
+            if (RegexValidation.ValidatingFirstName(firstName))
+            {
+                Console.WriteLine($"{firstName} Is Valid");
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} Is Not Valid");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexUsingLamda/RegexValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUsingLamda
+{
+    class RegexValidation
+    {
+        public const string FIRSTNAME = @"^[A-Z]{1}[a-z]{2,}";
+        public static bool ValidatingFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, FIRSTNAME);
+        }
+
+    }
+}
