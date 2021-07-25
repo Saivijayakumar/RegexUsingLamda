cf25eba24be48ff2ba80e0d243bd414b7579e352
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-25 17:03:00 +0530

    [SaiVijayaKumar] Using Lamda Function

--- RegexUsingLamda/RegexValidation.cs
+++ RegexUsingLamda/RegexValidation.cs
@@ -14,40 +14,10 @@ namespace RegexUsingLamda
         public const string EMAIL = @"^abc([+. \-_]{1}\w+)?@[a-z0-9]+\.[a-z]{2,3}(\.[a-z]{2,3})?$";
         public const string MOBILENUMBER = @"^[1-9]{2}[ ][0-9]{10}$";
         public const string PASSWORD = @"^[a-z]*[A-Z][a-z]*.[a-z]*[0-9][a-z]*$";
-        public static bool ValidatingFirstName(string firstName)
-        {
-            if (Regex.IsMatch(firstName,FIRSTNAME))
-                return true;
-            else
-                throw new InvalidCustomException(InvalidCustomException.ExceptionType.INVALID_FIRSTNAME, "Invalid First Name");
-        }
-        public static bool ValidatingLastName(string lastName)
-        {
-            if (Regex.IsMatch(lastName, LASTNAME))
-                return true;
-            else
-                throw new InvalidCustomException(InvalidCustomException.ExceptionType.INVALID_LASTNAME, "Invalid Last Name");
-        }
-        public static bool ValidatingEmail(string email)
-        {
-            if (Regex.IsMatch(email, EMAIL))
-                return true;
-            else
-                throw new InvalidCustomException(InvalidCustomException.ExceptionType.INVALID_EMAIL, "Invalid Email");
-        }
-        public static bool ValidatingMobileNumber(string mobileNumber)
-        {
-            if (Regex.IsMatch(mobileNumber, MOBILENUMBER))
-                return true;
-            else
-                throw new InvalidCustomException(InvalidCustomException.ExceptionType.INVALID_MOBILENUMBER, "Invalid Mobile Number");
-        }
-        public static bool ValidatingPassword(string password)
-        {
-            if (Regex.IsMatch(password, PASSWORD))
-                return true;
-            else
-                throw new InvalidCustomException(InvalidCustomException.ExceptionType.INVALID_PASSWORD, "Invalid Password");
-        }
+        public static bool ValidatingFirstName(string firstName) => Regex.IsMatch(firstName, FIRSTNAME);
+        public static bool ValidatingLastName(string lastName) => Regex.IsMatch(lastName, LASTNAME);
+        public static bool ValidatingEmail(string email) => Regex.IsMatch(email, EMAIL);
+        public static bool ValidatingMobileNumber(string mobileNumber) => Regex.IsMatch(mobileNumber, MOBILENUMBER);
+        public static bool ValidatingPassword(string password) => Regex.IsMatch(password, PASSWORD);
     }
 }
